4ba45ca656a17aaab9bd514982eada08aab5402b
matheus-pc <mag08081996@gmail.com>
2025-07-25 17:32:55 -0300

    Change compasiron of randomNumber
    randomNumber was compared like so: randomNumber < .3, randomNumber < .6, this would generate uneven random results, change that to the current implementation

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 function getComputerChoice() {
     let randomNumber = Math.random();
 
-    if (randomNumber < 0.3) {
+    if (randomNumber > 0 && randomNumber <= 0.3) {
         return "Rock";
-    } else if (randomNumber < 0.6) {
+    } else if (randomNumber <= 0.6) {
         return "Paper"
     } else {
         return "Scissor"
